d21bb7177c33737bd35d619c05118a6c12586625
Slaven Robic <slaven.robic@gmail.com>
2021-04-21 15:56:34 +0200

    random photo in details
    random photo in details

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,20 +5,19 @@ axios.defaults.baseURL = 'http://localhost:5000/api';
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
+const randomNumber = 5;
+const imageWidth = 480;
+const imageHeight = 480;
+const collectionID = 1163637;
+
 const requests = {
-    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    get: <T>(url: string) => axios.get<T>(url),
     post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-    del: <T>(url: string) => axios.delete<T>(url).then(responseBody)
 }
 
 const Hotels = {
     hotelsList: (params: any) => requests.post<Hotel[]>(`/hotels`, params),
-    list: () => requests.get<Hotel[]>('/hotels'),
-    details: (id: string) => requests.get<Hotel>(`/activities/${id}`),
-    create: (hotel: Hotel) => requests.post<Hotel>(`/activities`, hotel),
-    update: (hotel: Hotel) => requests.put<void>(`/activities/${hotel.name}`, hotel),
-    delete: (id: string) => requests.del<void>(`/activities/${id}`)
+    photos: () => requests.get<string>(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}`)
 }
 
 const Agent = {
--- client-app/src/features/activities/details/HotelDetails.tsx
+++ client-app/src/features/activities/details/HotelDetails.tsx
@@ -1,27 +1,45 @@
+import { useEffect, useRef } from 'react';
 import { useQueryClient } from 'react-query';
 import { Button, Card, Image } from 'semantic-ui-react'
+import agent from '../../../app/api/agent';
 import { Hotel } from '../../../app/model/Hotel'
 import { SELECTED_HOTEL, setSelectedHotel } from '../../../app/store/HotelStore';
 
 export const HotelDetails = () => {
     const queryClient = useQueryClient()
     const hotel: Hotel | undefined = queryClient.getQueryData<Hotel>(SELECTED_HOTEL)
+    const refs = useRef<HTMLInputElement>(null);
+
+    const getRandomImage = async () => {
+        let imageUrl = await agent.Hotels.photos()
+        let photo = imageUrl.config.url;
+        const img = refs.current?.children[0].children[0].children[0]
+        img?.setAttribute("src", photo!);
+    }
+
+    useEffect(() => {
+        getRandomImage()
+        return () => {
+        }
+    }, [hotel])
 
     return (
-        <Card fluid style={{ position: 'fixed', height: 'auto', width: '30em' }}>
-            <Image src='https://images.pexels.com/photos/6016679/pexels-photo-6016679.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' wrapped ui={false} />
-            <Card.Content>
-                <Card.Header>{hotel!.name}</Card.Header>
-                <Card.Meta>
-                    <span className='date'>{hotel!.name}</span>
-                </Card.Meta>
-                <Card.Description>
-                    {hotel!.description}
-                </Card.Description>
-            </Card.Content>
-            <Card.Content extra textAlign='right'>
-                <Button onClick={() => setSelectedHotel(undefined, queryClient)} basic color='red' content='Cancel' />
-            </Card.Content>
-        </Card >
+        <div ref={refs}>
+            <Card fluid style={{ position: 'fixed', height: 'auto', width: '30em' }}>
+                <Image wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>{hotel?.name}</Card.Header>
+                    <Card.Meta>
+                        <span className='date'>{hotel!.name}</span>
+                    </Card.Meta>
+                    <Card.Description>
+                        {hotel!.description}
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra textAlign='right'>
+                    <Button onClick={() => setSelectedHotel(undefined, queryClient)} basic color='red' content='Cancel' />
+                </Card.Content>
+            </Card >
+        </div>
     )
 }
